0523117883f26189bf478bc1d6e45f45bb0458ec
koram kram <zorojoru31@gmail.com>
2026-05-13 22:07:27 +0400

    create public repo and added code into repo

---
+++ parser/parse.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "parse.h"
+
+int is_digit(char c)
+{
+    return c >= 0x30 && c <= 0x39;
+}
+
+int parse(const char* string)
+{
+	
+    int i = 0;
+	int digitcast = 0;
+	
+    while (string[i] != '\0')
+    {
+        if (is_digit(string[i]))
+        {
+            digitcast = (INT)(string[i] - '0');
+			printf("\nresult : %d", digitcast);
+        }
+        else
+        {
+            printf("\nother: %c\n", string[i]);
+        }
+
+        i++;
+    }
+
+    return 0;
+}
+
+int main()
+{
+    char buff[1024];
+
+    printf("> ");
+
+    fgets(buff, sizeof(buff), stdin);
+
+    parse(buff);
+
+    return 0;
+}
---
+++ parser/parse.h
@@ -0,0 +1,12 @@
+#ifndef PARSE_H
+#define PARSE_H
+
+typedef int INT;
+typedef float FLOAT;
+typedef char CHARR;
+
+#define FAKEINT(x) ((int)(x))
+#define FAKEFLOAT(x) ((float)(x))
+#define FAKECHAR(x) ((char)(x))
+
+#endif

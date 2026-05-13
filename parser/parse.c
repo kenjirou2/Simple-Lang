eff9c206f324eafbaf5e8c734fe2cdf82c448a69
koram kram <zorojoru31@gmail.com>
2026-05-13 22:32:30 +0400

    added function is_float(); for float checking

--- parser/parse.c
+++ parser/parse.c
@@ -1,55 +1,39 @@
 #include <stdio.h>
-#include "parse.h"
 
-int is_digit(char c)
+int is_digit(const char* string)
 {
-    return c >= 0x30 && c <= 0x39;
+    return c >= '0' && c <= '9';
 }
 
 int digit(const char* string)
 {
-	return *string - '0';
+	return string - '0';
 }
 
-int is_float()
-{
-	
-	
-}
-
-int parse(const char* string)
+int is_float(const char* string)
 {
 	
     int i = 0;
-	int digitcast = 0;
-	
-    while (string[i] != '\0')
-    {
-        if (is_digit(string[i])
-        {
-            digitcast = (INT)(digit(string[i]));
-			printf("\nresult : %d", digitcast);
-        }
-        else
-        {
-            printf("\nother: %c\n", string[i]);
-        }
 
-        i++;
+    while (is_digit(*string))
+    {
+        i = 1;
+        string++;
     }
 
-    return 0;
-}
 
-int main()
-{
-    char buff[1024];
+    if (*string == '.')
+    {
+        string++;
+        if (!is_digit(*string)) { return 0; }
 
-    printf("> ");
+        while (is_digit(*s))
+		{
+			string++;
+		}
+        
+        return 1;
+    }
 
-    fgets(buff, sizeof(buff), stdin);
-
-    parse(buff);
-
-    return 0;
+    return i;
 }
